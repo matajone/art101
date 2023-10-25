3f7a9ad6c316e598d655dc0df82e715239049638
matajone <Mailejonessart@gmail.com>
2023-10-24 22:59:05 -0700

    Lab 5 changes

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,23 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// * Author: Maile Jones
+// * Created: 24 October 2023
+// * liscence: Public Domain
 
-// Constants
 
-// Functions
+// * Define Variables *
+make="honda";
+model="civic";
+year=2009;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
 
-// let's get this party started
-main();
+// calculate
+
+age=2023 - year;
+
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Year" + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
+
