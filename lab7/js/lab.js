ad445caa790f97e82482764591922693c5e2a012
matajone <Mailejonessart@gmail.com>
2023-11-01 21:49:19 -0700

    update

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -2,16 +2,25 @@
 // Author: Maile Jones
 // Date:10/31/2023
 
+//
+// Code structure from Wes Modes
+
 // userNameSort- function that sorts the letters in a user's name
 function userNameSort() {
     var userName = window.prompt(" Hello, enter username for reconfiguration.");
     console.log("userName =",userName);
+
+    //
     // Split string to array 
     var nameArray = userName.split('');
     console.log("nameArray =", nameArray);
+
+    //
     // Sort the array
     var nameArraySort = nameArray.sort();
     console.log("nameArraySort =", nameArraySort);
+
+    //
     // Turn array back into string
     var nameSorted = nameArraySort.join('');
     console.log("nameSorted =", nameSorted);
