82c7c9e7474071ac8957f98812bb384328dced86
matajone <Mailejonessart@gmail.com>
2023-12-07 21:47:47 -0800

    lab 16 edits

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -2,18 +2,14 @@
 // Date: 12-05-2023
 //
 console.log("js loaded");
-
 $.ajax({
-    url:"https://xkcd.com/5/info.0.json",
+    url:"https://api.chucknorris.io/jokes/random",
     method: "GET",
     dataType: "json",
     data: {},
     success: function (data){
         console.log(data);
-        $("#output").append(data.month+"-"+data.day+"-"+data.year+"</br>");
-        $("#output").append(data.img);
-        $("#output").append(data.alt);
-        $("#output").after("<img src="+">");
+        $("#output").append(data.value);
     },
     error: function (jqXHR, textStatus, errorThrown){
         console.log("error:",textStatus,errorThrown);
