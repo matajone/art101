91d95cb5b4ed2ae3c70e3d84c343bfa23bf41b80
matajone <Mailejonessart@gmail.com>
2023-12-10 20:10:14 -0800

    group project edits

--- groupproject/js/test.js
+++ groupproject/js/test.js
@@ -1,41 +1,101 @@
-/*
-var myButton = document.getElementById("button");
-myButton.addEventListener("click",function (){
-    var name = document.getElementById("input").value;
-    var house = sortingHat(name);
-    newText ="<p> The sorting hat has sorted you into " + house + "</p>";
-    document.getElementById("output").innerHTML = newText;
-});
-*/
+ 
+function main() {
+  GetValues();
+} 
 
+var early;
+var ontime;
+var late;
 
-/*function createObjectFromSelect(){
-    const select = document.getElementById('stop');
-    const select = document.getElementById('route');
+function GetValues(){
     
-    const selectedOption = select.options[selected.selectedIndex];
+    //gets values
+    var settings = {
+      "url": "https://getpantry.cloud/apiv1/pantry/51f361dd-ba2d-411b-aaf1-61b2ecb6064b/basket/bus",
+      "method": "GET",
+      "timeout": 0,
+      "headers": {
+        "Content-Type": "application/json"
+      },
+    };
+    
+    $.ajax(settings).done(function (response) {
+      console.log(response);
 
-    const selectedObject = {
-        busStop: selectedOption.stop,
-        
+      early = response.early;
+      ontime = response.ontime;
+      late = response.late;
 
-    }
-}*/
+      timeChart = new Chart( "timeChart", {
+        type: "bar",
+        data: {
+            labels: ["Early","OnTime","Late"],
+            datasets: [{
+            backgroundColor: ["red", "green","blue"],
+                //data: [Math.floor(Math.random() * 30), Math.floor(Math.random() * 40), Math.floor(Math.random() * 50)]
+                data: [early, ontime, late]
+            }]
+        },
+        options: {scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero: true
+              }
+          }]
+      }} });
+  
+      console.log("early, ontime, late: " + early + " " + ontime + " " + late);
+    });
+  
+  }
 
-
-const timeChart = new Chart( "timeChart", {
+  
+  var timeChart = new Chart( "timeChart", {
     type: "bar",
     data: {
         labels: ["Early","OnTime","Late"],
         datasets: [{
         backgroundColor: ["red", "green","blue"],
-            data: [Math.floor(Math.random() * 30), Math.floor(Math.random() * 40), Math.floor(Math.random() * 50)]
+            //data: [Math.floor(Math.random() * 30), Math.floor(Math.random() * 40), Math.floor(Math.random() * 50)]
+            data: [early, ontime, late]
         }]
     },
-    options: {}
-})
-console.log("Chart is" + timeChart);
-$("#output").append(timeChart);
+    options: {scales: {
+      yAxes: [{
+          ticks: {
+              beginAtZero: true
+          }
+      }]
+  }} })
+  
+  function IncrementValues(e, o, l){
+    
+    console.log("json stringify is: " + JSON.stringify({
+      "early": (early + e),
+      "ontime": (ontime + o), 
+      "late": (late + l)}));
+  
+    //updates values
+    var settings = {
+      "url": "https://getpantry.cloud/apiv1/pantry/51f361dd-ba2d-411b-aaf1-61b2ecb6064b/basket/bus",
+      "method": "PUT",
+      "timeout": 0,
+      "headers": {
+        "Content-Type": "application/json"
+      },
+      "data": JSON.stringify({
+        "early": (early + e),
+        "ontime": (ontime + o), 
+        "late": (late + l),
+      }),
+    };
+    
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    });
+  }
+
+//$("#output").append(timeChart);
 
 
 function getData(){
@@ -44,10 +104,42 @@ function getData(){
 
     var bussto = $("#stop").val();
     console.log("Bus Stop Location is: " + bussto);
-
+    
     var busarr = $("#arrive").val();
     console.log("Bus arrive status is: " + busarr);
     
     var busdata = [busrou, bussto, busarr];
+
+    switch($("#arrive").val()){
+        case "early":
+            IncrementValues(1,0,0);
+            break;
+        case "ontime":
+            IncrementValues(0,1,0);
+            break;
+        case "late":
+            IncrementValues(0,0,1);
+            break;
+    }
+
     return busdata;
 }
+
+$("#party-toggle button").click(function(){
+    if ($("body").hasClass("party")) {
+      $("body").removeClass("party"); 
+      location.reload(); 
+    }
+    else {
+      $("body").addClass("party");
+      $("body").append("Thank you for your input!");
+      debugger;
+    }
+  })
+
+$.ajax({
+   url: "https://getpantry.cloud/apiv1/pantry/9532ecda-611e-4d74-4d74-ad60-0460-0460e26a6af4",
+
+})
+
+main();
